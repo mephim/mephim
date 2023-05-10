c5fa1ad77a5f515bd13ce60607483576cff78b23
lchau1 <leconghau095@gmail.com>
2023-05-10 17:43:10 +0700

    update edit movie controller

--- src/main/java/com/example/mephim/request/LoginRequest.java
+++ src/main/java/com/example/mephim/request/LoginRequest.java
@@ -2,9 +2,6 @@ package com.example.mephim.request;
 
 import lombok.*;
 
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
+public interface LoginRequest {
 }
 
